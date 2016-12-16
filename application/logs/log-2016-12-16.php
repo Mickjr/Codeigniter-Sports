e622eeb0748665cef9078bd9d145cd45df77a88d
Horton Jr., Manton <MHorton@bgea.org>
2016-12-16 10:18:28 -0500

    update application folder

---
+++ application/logs/log-2016-12-16.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-16 10:14:23 --> 404 Page Not Found: /index
+ERROR - 2016-12-16 10:14:23 --> 404 Page Not Found: /index
